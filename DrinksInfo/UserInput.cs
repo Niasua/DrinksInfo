8656dafe1d903ab02f7c8d983512dedbe35fddbf
niasua <nsuarez2410@gmail.com>
2025-07-22 20:15:29 -0300

    drinks are shown correctly

--- DrinksInfo/UserInput.cs
+++ DrinksInfo/UserInput.cs
@@ -1,10 +1,49 @@
-﻿namespace DrinksInfo;
+﻿
+namespace DrinksInfo;
 public class UserInput
 {
-    DrinkService drinkService = new();
+    DrinksService drinksService = new();
 
     internal void GetCategoriesInput()
     {
-        drinkService.GetCategories();
+        drinksService.GetCategories();
+
+        Console.WriteLine("Choose category:");
+
+        string category = Console.ReadLine();
+
+        while (!Validator.IsStringValid(category))
+        {
+            Console.WriteLine("\nInvalid category");
+            category = Console.ReadLine();
+        }
+
+        GetDrinksInput(category);
+    }
+
+    private void GetDrinksInput(string category)
+    {
+        drinksService.GetDrinksByCategory(category);
+
+        Console.WriteLine("Choose a drink or go back to category menu by typing 0:");
+
+        string drink = Console.ReadLine();
+
+        if (drink == "0") GetCategoriesInput();
+
+        while (!Validator.IsIdValid(drink))
+        {
+            Console.WriteLine("\nInvalid drink");
+            drink = Console.ReadLine();
+        }
+
+        drinksService.GetDrink(drink);
+
+        Console.WriteLine("\nPress ENTER to choose another drink, or type 0 to return to categories:");
+        string next = Console.ReadLine();
+        if (next == "0")
+            GetCategoriesInput();
+        else
+            GetDrinksInput(category);
     }
 }
